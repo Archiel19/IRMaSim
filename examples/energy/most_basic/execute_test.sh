f16dc76eb4a986e82c031f48bc3e8d7e829c0e5e
Archiel19 <you@example.com>
2023-03-07 12:16:04 +0100

    Improved the testing script a bit.

--- examples/energy/most_basic/execute_test.sh
+++ examples/energy/most_basic/execute_test.sh
@@ -1,25 +1,58 @@
-# TODO input error handling
-PHASE=$1
-OPT=""
+PYTHON=python3.9
+ITERS=10
 
-if [ $PHASE == "train" ]
-then
+# Defaults
+energy_only="false"
+phase="test"
+
+usage() {
+  printf "Usage: %+18s [-p <train|test>] [-e]\n" "$0" 1>&2
+  printf "%+9sp: phase (default: test)\n%+9se: energy agent only (default: false)\n" "-" "-" 1>&2
+  exit 1
+}
+
+while getopts "p:eh" opt; do
+  case ${opt} in
+  p)
+    phase=$OPTARG
+    if [ "$phase" != "train" ] && [ "$phase" != "test" ]; then
+      printf "%s: invalid argument for option -- p\n" "$0" 1>&2
+      usage
+    fi
+    ;;
+  e)
+    energy_only="true"
+    ;;
+  h | * )
+    usage
+    ;;
+  esac
+done
+
+printf "Phase: %s\n\n" "$phase"
+
+plot_opt="-r"
+if [ "$phase" == "train" ]; then
   rm -f policy_agent.model energy_agent.model action_agent.model
-  OPT="-l"
+  plot_opt=${plot_opt}" -l"
 fi
 rm -f {low_power,high_gflops,policy,action,energy}/*
+
+if [ "$energy_only" = "true" ]; then
+  echo "Using energy agent only"
+  irmasim -nr ${ITERS} -im energy_agent.model -om energy_agent.model options_energy.json --phase "$phase"
+  ${PYTHON} plotter.py -d energy ${plot_opt}
+  exit 0
+fi
 
 printf 'Low power heuristic\n'
 irmasim options_low_power.json
 printf '\nHigh gflops heuristic\n'
 irmasim options_high_gflops.json
-printf '\nPolicy agent\n'
-irmasim -nr 10 -im policy_agent.model -om policy_agent.model options_policy.json --phase $PHASE
-printf '\nActionActorCritic agent\n'
-irmasim -nr 10 -im action_agent.model -om action_agent.model options_action.json --phase $PHASE
-printf '\nEnergy agent\n'
-irmasim -nr 10 -im energy_agent.model -om energy_agent.model options_energy.json --phase $PHASE
 
-python3.9 plotter.py -d energy -r $OPT
-python3.9 plotter.py -d policy -r $OPT
-python3.9 plotter.py -d action -r $OPT
+AGENTS=("policy" "action" "energy")
+for type in "${AGENTS[@]}"; do
+  printf "\n%s agent\n" "$type"
+  irmasim -nr ${ITERS} -im "$type"_agent.model -om "$type"_agent.model options_"$type".json --phase "$phase"
+  ${PYTHON} plotter.py -d "$type" ${plot_opt}
+done
