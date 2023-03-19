670a9123db26b1c256978498e4c955dcfc9e3bc0
Archiel19 <you@example.com>
2023-03-19 17:07:53 +0100

    Update test structure and script. - Reorganize option file locations. - Add option to specify the number of simulations to run. - Add high_cores heuristic. - Remove generic_test.

--- examples/energy/execute_test.sh
+++ examples/energy/execute_test.sh
@@ -1,22 +1,23 @@
 PYTHON=python3.9
-ITERS=7
 
 # Defaults
 energy_only="false"
 phase="train"
 dir=""
 plot_opt="-r"
+iters=10
 
 # Input handling
 usage() {
   printf "Usage: %+18s -d <directory> [-p <train|test>] [-e] [-h]\n" "$0" 1>&2
   printf "%+9sp: phase (default: %s)\n" "-" "$phase" 1>&2
   printf "%+9se: energy agent only (default: %s)\n" "-" "$energy_only" 1>&2
+  printf "%+9si: number of simulations to run (default: %d)\n" "-" "$iters" 1>&2
   printf "%+9sh: display this message and exit\n" "-" 1>&2
   exit 1
 }
 
-while getopts "d:p:eh" opt; do
+while getopts "d:p:i:eh" opt; do
   case ${opt} in
   p)
     phase=$OPTARG
@@ -27,6 +28,9 @@ while getopts "d:p:eh" opt; do
     ;;
   d)
     dir=$OPTARG
+    ;;
+  i)
+    iters=$OPTARG
     ;;
   e)
     energy_only="true"
@@ -49,26 +53,27 @@ if [ "$phase" == "train" ]; then
   rm -f policy_agent.model action_agent.model energy_agent.model
   plot_opt=${plot_opt}" -l"
 fi
-rm -f {low_power,high_gflops,policy,action,energy}/*
+rm -f {energy,policy,action,low_power,high_gflops,high_cores}/*
+
 
 # Execution
-if [ "$energy_only" = "true" ]; then
-  echo "Using energy agent only"
-  irmasim -nr ${ITERS} -im energy_agent.model -om energy_agent.model "$prev_dir"/options_energy.json --phase "$phase"
-  ${PYTHON} "$prev_dir"/plotter.py -d energy ${plot_opt}
-  exit 0
+if [ "$energy_only" != "true" ]; then
+  printf 'Low power heuristic\n'
+  irmasim "$prev_dir"/options_low_power.json
+  printf '\nHigh gflops heuristic\n'
+  irmasim "$prev_dir"/options_high_gflops.json
+  printf '\nHigh cores heuristic\n'
+  irmasim "$prev_dir"/options_high_cores.json
+  AGENTS=("policy" "action")
+  for type in "${AGENTS[@]}"; do
+    printf "\n%s agent\n" "$type"
+    irmasim -nr "$iters" -im "$type"_agent.model -om "$type"_agent.model options_"$type".json --phase "$phase"
+    ${PYTHON} "$prev_dir"/plotter.py -d "$type" ${plot_opt}
+  done
 fi
 
-printf 'Low power heuristic\n'
-irmasim ../options_low_power.json
-printf '\nHigh gflops heuristic\n'
-irmasim ../options_high_gflops.json
-
-AGENTS=("policy" "action" "energy")
-for type in "${AGENTS[@]}"; do
-  printf "\n%s agent\n" "$type"
-  irmasim -nr ${ITERS} -im "$type"_agent.model -om "$type"_agent.model "$prev_dir"/options_"$type".json --phase "$phase"
-  ${PYTHON} "$prev_dir"/plotter.py -d "$type" ${plot_opt}
-done
+printf '\nEnergy agent\n'
+irmasim -nr "$iters" -im energy_agent.model -om energy_agent.model options_energy.json --phase "$phase"
+${PYTHON} "$prev_dir"/plotter.py -d energy ${plot_opt}
 
 cd "$prev_dir" || exit 1
--- examples/energy/generic_test/execute_test.sh
+++
@@ -1,4 +0,0 @@
-rm -f agent.model test/*
-mkdir -p test
-irmasim -nr 15 -im agent.model -om agent.model options_test.json
-python3.9 plotter.py -d test -r -l
