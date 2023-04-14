0b099a246ac68f2b18b49d5046f19250a51bfe8e
Archiel19 <you@example.com>
2023-04-14 13:44:15 +0200

    Add functionality to plot resource usage.

--- examples/energy/plotter.py
+++ examples/energy/plotter.py
@@ -27,12 +27,43 @@ def plot_rewards(exp_dir: str):
     plt.clf()
 
 
-def do_plots(args):
+def plot_resources(exp_dir: str):
+    with open(f'{exp_dir}/resources.log', 'r') as r_f:
+        lines = [tuple(rl.strip().split(',')) for rl in r_f.readlines()]
+        indices = {name: idx for (idx, name) in enumerate(lines[0])}
+        def get(line, tag):
+            return line[indices[tag]]
 
+        # Only plot the last run
+        last_run = get(lines[-1], 'run')
+        lines = [line for line in lines[1:] if line[indices['run']] == last_run]
+
+        # Because we don't want to assume consecutive resource IDs
+        first_id = get(lines[0], 'id')
+        max_capacity = int(get(lines[0], 'cores'))
+        chunk_size = 1
+        for line in lines[1:]:
+            if get(line, 'id') == first_id:
+                break
+            chunk_size += 1
+            max_capacity += int(get(line, 'cores'))
+        # TODO get the last trajectory only
+        timestamps = [float(get(lines[i], 'time')) for i in range(0, len(lines), chunk_size)]
+        loads = [sum(map(lambda l: int(get(l, 'busy_cores')), lines[i:i+chunk_size]))
+                 for i in range(1, len(lines), chunk_size)]
+
+    plt.plot(timestamps, loads)
+    plt.axhline(y=max_capacity, linestyle='dashed', color='r')
+    plt.savefig(f'{exp_dir}/resources.png')
+    plt.clf()
+
+
+def do_plots(args):
     if args.loss:
         plot_losses(args.exp_dir)
     if args.reward:
         plot_rewards(args.exp_dir)
+    plot_resources(args.exp_dir)
 
 
 if __name__ == '__main__':
